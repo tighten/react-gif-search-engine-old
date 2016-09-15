f66621b9d21284aed1d1e3a9bf5052694d836346
Samantha Geitz <samanthamichele7@gmail.com>
2016-09-15 11:19:32 -0500

    Step 15: Add login and signup form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import GifsReducer from './gifs';
 import ModalReducer from './modal';
+import { reducer as FormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   gifs: GifsReducer,
   modal: ModalReducer
 });
